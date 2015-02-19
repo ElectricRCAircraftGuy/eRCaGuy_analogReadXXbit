357af26870e2d3f71099183adc846badfed5200a
Gabriel Staples <ercaguy@gmail.com>
2015-02-18 22:23:26 -0500

    fixed bugs in the "eight_analog_reads"  speed test

--- examples/Speed Tests/eight_analog_reads/eight_analog_reads.ino
+++ examples/Speed Tests/eight_analog_reads/eight_analog_reads.ino
@@ -1,6 +1,7 @@
 /*
 eight_analog_reads.ino
 -rapidly read A0 through A7, on an ATmega328 SMT chip (ex: Arduino Nano, Pro Mini, etc, but NOT Uno), and measure the time this takes
+--you may set global variable "NUM_ANALOG_PINS" below to 6 instead of 8 if you want to use this code on an Arduino Uno
 -do this at a higher resolution (ex: 12-bit, via oversampling)
 
 By Gabriel Staples
@@ -37,7 +38,7 @@ Updated: 18 Feb. 2015
 #include <eRCaGuy_Timer2_Counter.h> //for measuring time to a 0.5us resolution (instead of the 4us resolution that micros() has)
 
 //Global Vars
-const byte numAnalogPins = 8;
+const byte NUM_ANALOG_PINS = 8; //set to 6 if using an Arduino Uno or other ATmega328 that only has 6 analog pins; set to 8 for Nano, Pro Mini, etc.
 
 void setup()
 {
@@ -47,24 +48,25 @@ void setup()
   //Configure ADC
   adc.setBitsOfResolution(12);
   
-  //****************THIS ADC SPEED RESULTS IN ~780us PER 12-BIT ANALOG SAMPLE****************(1282 Hz sample rate)
-  adc.setADCSpeed(ADC_FAST); 
+  //****************THIS ADC SPEED RESULTS IN ~390us PER 12-BIT ANALOG SAMPLE (~2565 Hz sample rate)****************
+//  adc.setADCSpeed(ADC_FAST); 
   
-  //****************THIS ADC SPEED RESULTS IN ~550us PER 12-BIT ANALOG SAMPLE****************(1818 Hz sample rate)
-  //adc.setADCSpeed(CAUTION_ADC_PRESCALER_8_CLOCK_2MHZ); //this speed is 2x that of ADC_FAST, and should still produce good results too; 
+  //****************THIS ADC SPEED RESULTS IN ~275us PER 12-BIT ANALOG SAMPLE (~3636 Hz sample rate)****************
+  adc.setADCSpeed(CAUTION_ADC_PRESCALER_8_CLOCK_2MHZ); //this ADC clock speed is 2x that of ADC_FAST, and should still produce good results too; 
                                                        //give it a try, though it may reduce actual analog resolution a touch
 }
 
 void loop()
 {
   //local variables
-  unsigned int readings[numAnalogPins]; //prepare array to receive 8 analog readings
-  unsigned long times[numAnalogPins]; //units of 0.5us
-  unsigned long t_start = timer2.get_count(); //count units of 0.5us each
+  unsigned int readings[NUM_ANALOG_PINS]; //prepare array to receive 8 analog readings
+  unsigned long times[NUM_ANALOG_PINS]; //units of 0.5us
+  float times_f[NUM_ANALOG_PINS]; //us; floating point times
   
   //take readings as fast as possible
   byte i = 0;
-  for (byte pin=A0; pin<=A7; pin++) //for analog pins A0 through A7
+  unsigned long t_start = timer2.get_count(); //count units of 0.5us each
+  for (byte pin=A0; pin<A0+NUM_ANALOG_PINS; pin++) //for analog pins A0 through A5 (Uno) or A7 (Nano)
   {
     analogRead(pin); //take a single reading and throw it out, to let the ADC settle
     readings[i] = adc.newAnalogRead(pin);
@@ -74,28 +76,27 @@ void loop()
   
   //do time calcs & print out results
   i = 0;
-  for (byte pin=A0; pin<=A7; pin++) //for analog pins A0 through A7
+  for (byte pin=A0; pin<A0+NUM_ANALOG_PINS; pin++)
   {
     //zero-reference the times, so that each time value is an elapsed time since starting this data set
-    times[i] = times[i] - t_start; //0.5us units
-    //get us value of time elapsed
-    float t_elapsed = (float)times[i]/2.0; //us; convert to us
+    times_f[i] = (float)(times[i] - t_start)/2.0; //us; the "/2.0" is to convert from 0.5us units to us
     //get time for just this one analog reading
     float dt; //us
     if (i>0)
     {
-      dt = times[i] - times[i-1]; //us
+      dt = times_f[i] - times_f[i-1]; //us
     }
     else //i==0
     {
-      dt = times[i]; //us
+      dt = times_f[i]; //us
     }
     
     //print results
     Serial.print(F("pin = A")); Serial.print(pin-14); //subtract 14 since A0 is actually Arduino pin 14, but I want it to print out as "A0" instead of "14"
     Serial.print(F(", reading = ")); Serial.print(readings[i]); 
-    Serial.print(F(", t_elapsed(us) = ")); Serial.print(times[i]);
-    Serial.print(F(", dt(us) = ")); Serial.println(dt);
+    Serial.print(F(", t_elapsed(us) = ")); Serial.print(times_f[i]);
+    Serial.print(F(", dt(us) = ")); Serial.print(dt);
+    Serial.print(F(", ")); Serial.print(adc.getBitsOfResolution()); Serial.print(F("-bit sample rate(Hz) = ")); Serial.println(1000000/dt);
     i++;
   }
   
